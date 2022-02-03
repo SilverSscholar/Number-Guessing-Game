c73090b8af477375d7b048ef4f132e03ab71c266
Steve Jones <jones.wiredbraincoffee@gmail.com>
2022-02-03 13:04:40 -0500

    Add project files.

---
+++ Number Guessing Game/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+
+            var winno = ran.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 0, and 100");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if(i>winno)
+                {
+                    Console.WriteLine("Your answer was too high! Please guess lower...");
+                }
+                else if(i<winno)
+                {
+                    Console.WriteLine("Your answer was to low, please guess higher...");
+                }
+
+                else if(i==winno)
+                {
+                    Console.WriteLine("Congragulations you guessed the correct answer!");
+                    win = true;
+                }
+
+                Console.WriteLine();
+
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing the game!");
+            Console.WriteLine("Press any key to finish");
+            Console.ReadKey(true);
+        }
+    }
+}
